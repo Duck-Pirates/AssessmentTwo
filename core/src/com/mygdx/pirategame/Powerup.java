b13d62f3f070cf634b4179893ce74d4439e12fce
brexit2001 <davidebressani21@gmail.com>
2022-03-05 12:08:03 +0000

    A2-6 A2-57 Added a Powerup entity defitionition, while also adding some logic for a future better implementation of a collision system

--- core/src/com/mygdx/pirategame/Powerup.java
+++ core/src/com/mygdx/pirategame/Powerup.java
@@ -3,6 +3,9 @@ package com.mygdx.pirategame;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 
 /**
  * This class implements methods and variables for every powerup in game, that will have their own class and methods too
@@ -55,4 +58,27 @@ public abstract class Powerup extends Entity{
         }
     }
 
+    /**
+     * Defines all the parts of the coins physical model. Sets it up for collisons
+     */
+    @Override
+    protected void defineEntity() {
+        //sets the body definitions
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(getX(), getY());
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        b2body = world.createBody(bdef);
+
+        //Sets collision boundaries
+        FixtureDef fdef = new FixtureDef();
+        CircleShape shape = new CircleShape();
+        shape.setRadius(24 / PirateGame.PPM);
+        // setting BIT identifier
+        fdef.filter.categoryBits = PirateGame.POWERUP_BIT;
+        // determining what this BIT can collide with
+        fdef.filter.maskBits = PirateGame.DEFAULT_BIT | PirateGame.PLAYER_BIT | PirateGame.ENEMY_BIT;
+        fdef.shape = shape;
+        fdef.isSensor = true;
+        b2body.createFixture(fdef).setUserData(this);
+    }
 }
