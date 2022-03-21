d453254cad106a18fadadf7e6808fd9a7aa6900c
brexit2001 <davidebressani21@gmail.com>
2022-03-21 18:19:48 +0000

    Added this change to check the hit-boxes

--- core/src/com/mygdx/pirategame/GameScreen.java
+++ core/src/com/mygdx/pirategame/GameScreen.java
@@ -418,7 +418,7 @@ public class GameScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         renderer.render();
         // b2dr is the hitbox shapes, can be commented out to hide
-        //b2dr.render(world, camera.combined);
+        b2dr.render(world, camera.combined);
 
         game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
