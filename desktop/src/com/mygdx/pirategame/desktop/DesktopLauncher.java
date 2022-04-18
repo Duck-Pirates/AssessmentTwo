883cbf4a6cfc8085cd0b6a20e12010c024aededb
brexit2001 <davidebressani21@gmail.com>
2022-04-18 12:56:14 +0100

    A2-88 Using the solution found on StackOverflow, still gives back error

--- desktop/src/com/mygdx/pirategame/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/pirategame/desktop/DesktopLauncher.java
@@ -1,15 +1,21 @@
 package com.mygdx.pirategame.desktop;
 
 import java.awt.*;
+import java.nio.IntBuffer;
+
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.mygdx.pirategame.PirateGame;
+import org.lwjgl.glfw.GLFW;
 
 public class DesktopLauncher {
-	static Rectangle availableArea = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
 	public static void main (String[] arg) {
+		IntBuffer xpos_b, ypos_b, width_b, height_b;
+		int value = 1;
+		xpos_b = ypos_b = width_b = height_b = IntBuffer.allocate(value);
+		GLFW.glfwGetMonitorWorkarea(GLFW.glfwGetPrimaryMonitor(), xpos_b, ypos_b, width_b, height_b);
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setWindowedMode(availableArea.width-300, availableArea.height-300);
+		config.setWindowedMode(1400, 1400);
 		new Lwjgl3Application(new PirateGame(), config);
 	}
 }
